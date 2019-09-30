f99989af2e1da690bfa72b4678c53d6d30781b72
barbara-chaves <barbarabarcellos90@gmail.com>
2019-09-30 14:29:48 +0200

    create dev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import getDataFromServer from './data/getDataFromServer'
 
 function App() {
+  getDataFromServer('Madrid').then(data => console.log(data))
+  getDataFromServer('Barcelona').then(data => console.log(data))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
