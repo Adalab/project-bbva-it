0f23d465a8c4d81ea380ca827e8bd0bdb9cdc07f
btdvalle <btdvalle@gmail.com>
2019-10-01 17:49:31 +0200

    Add city button

--- src/WeatherWidget.js
+++ src/WeatherWidget.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CityButton from "./components/CityButton";
 const getCurrentWeather = require("./data/serviceCurrentWeather").getCurrentWeather;
 const getForecastWeather = require("./data/serviceForecastWeather").getForecastWeather;
 
@@ -6,16 +7,28 @@ class WeatherWidget extends React.Component {
   constructor() {
     super();
     this.state = {
+      currentCity: "Madrid",
       currentWeather: {}
     };
+    this.changeCity = this.changeCity.bind(this);
   }
 
   componentDidMount() {
-    getCurrentWeather("Madrid").then(currentWeather => this.setState({ currentWeather }));
+    getCurrentWeather(this.state.currentCity).then(currentWeather => this.setState({ currentWeather }));
+  }
+
+  changeCity(currentCity) {
+    this.setState({ currentCity }, this.componentDidMount);
   }
 
   render() {
-    return <div className="WeatherWidget"></div>;
+    const { currentCity } = this.state;
+    return (
+      <div className="WeatherWidget">
+        <h2>{currentCity}</h2>
+        <CityButton currentCity={currentCity} changeCity={this.changeCity} />
+      </div>
+    );
   }
 }
 
