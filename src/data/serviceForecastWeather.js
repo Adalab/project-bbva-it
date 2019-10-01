58244fd9efa5332328d066d81995ea86a68f2c29
barbara-chaves <barbarabarcellos90@gmail.com>
2019-10-01 22:36:37 +0200

    create service functions

--- src/data/serviceForecastWeather.js
+++ src/data/serviceForecastWeather.js
@@ -1,40 +1,95 @@
-const getForecastWeather = city => {
-  const getCityId = () => {
-    if (city === "Madrid") {
-      return "3117735";
-    } else if (city === "Barcelona") {
-      return "3128760";
-    }
-  };
+const getCityId = city => {
+  if (city === "Madrid") {
+    return "3117735";
+  } else if (city === "Barcelona") {
+    return "3128760";
+  }
+};
 
-  const endpoint = `http://api.openweathermap.org/data/2.5/forecast?id=${getCityId(city)}&APPID=0c63c7586eaed6d88005102690c6ec7b`;
+const getForecastWeather = city => {
+  const endpoint = `http://api.openweathermap.org/data/2.5/forecast?id=${getCityId(
+    city
+  )}&APPID=0c63c7586eaed6d88005102690c6ec7b`;
 
   return fetch(endpoint)
     .then(response => response.json())
     .then(data => formatData(data));
 };
 
-const selectData = data => {
-  const arr = data.list.slice(8, 32);
-  console.log(arr);
-  const day2 = arr.slice(0);
+const convertTemp = number => parseInt(number - 273.15);
+
+const getMax = arr => {
+  const max = arr.reduce((acc, curr) =>
+    curr.main.temp > acc.main.temp ? curr : acc
+  ).main.temp;
+  return convertTemp(max);
 };
 
-const formatData = data => {
-  selectData(data);
-  console.log(data);
-  //   const convertedTemp = parseInt(data.main.temp - 273.15);
-  return {
-    // country: data.sys.country,
-    // city: data.name,
-    // temp: convertedTemp,
-    // max: data.main.temp_max,
-    // min: data.main.temp_min,
-    // humidity: data.main.humidity,
-    // description: data.weather[0].description,
-    // icon: data.weather[0].icon,
-    // wind: data.wind.speed
+const getMin = arr => {
+  const min = arr.reduce((acc, curr) =>
+    curr.main.temp < acc.main.temp ? curr : acc
+  ).main.temp;
+  return convertTemp(min);
+};
+
+const getIcon = arr => {
+  const iconArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const icon = arr[i].weather[0].icon;
+    let count = 0;
+
+    for (let f = 0; f < arr.length; f++) {
+      if (arr[f].weather[0].icon === icon) {
+        count += 1;
+      }
+    }
+
+    let mostRepeated = { icon: icon, repeated: count };
+
+    iconArr.push(mostRepeated);
+  }
+  const arr2 = iconArr.reduce((acc, curr) =>
+    acc.repeated > curr.repeated ? acc : curr
+  );
+  return arr2.icon
+};
+
+const getDay = (date) => {
+  let day = date.split(' ')[0].split('-')[2] + '-' + date.split(' ')[0].split('-')[1];
+  return day
+};
+
+const days = day => {
+  return { 
+      max: getMax(day), 
+      min: getMin(day), 
+      icon: getIcon(day),
+      day: getDay(day[0].dt_txt) 
   };
 };
 
-module.exports = { getForecastWeather, formatData };
+
+const selectData = data => {
+  return data.list.slice(8, 32);
+};
+
+const formatData = data => {
+  let arr = selectData(data);
+  let day2 = arr.slice(0, 8);
+  let day3 = arr.slice(8, 16);
+  let day4 = arr.slice(16, 24);
+  
+  return [days(day2), days(day3), days(day4)];
+};
+
+module.exports = {
+  getForecastWeather,
+  formatData,
+  selectData,
+  convertTemp,
+  getMax,
+  getMin,
+  getIcon,
+  getDay
+};
--- src/test/ServiceForecastWeather.test.js
+++ src/test/ServiceForecastWeather.test.js
@@ -1,9 +1,75 @@
+const fakeForecastFetch = require("../mock/mockForecast.json");
 const formatData = require("../data/serviceForecastWeather").formatData;
-const fakeFetch = require("../mock/mock.json");
+const selectData = require("../data/serviceForecastWeather").selectData;
+const convertTemp = require("../data/serviceForecastWeather").convertTemp;
+const getMax = require("../data/serviceForecastWeather").getMax;
+const getMin = require("../data/serviceForecastWeather").getMin;
+const getIcon = require("../data/serviceForecastWeather").getIcon;
+const getDay = require("../data/serviceForecastWeather").getDay;
 
 describe("get forecast weather data from server", () => {
   it("returns an object", () => {
-    const result = formatData(fakeFetch);
+    const result = formatData(fakeForecastFetch);
     expect(typeof result).toBe("object");
+  });
+});
+
+describe("select 3 days of forecast weather data from server", () => {
+  it("returns an array with 24 objects", () => {
+    const result = selectData(fakeForecastFetch).length;
+    const expected = 24
+    expect(result).toBe(expected);
+  });
+});
+
+describe("convert temperature kelvin - celsius", () => {
+  it("returns 0  when 273.15", () => {
+    const result = convertTemp(273.15);
+    const expected = 0
+    expect(result).toBe(expected);
+  });
+});
+
+describe("get the max temperature of the day", () => {
+  it("returns 26 on the first day", () => {
+    const day = fakeForecastFetch.list.slice(8, 16);
+    const result = getMax(day);
+    const expected = 26
+    expect(result).toBe(expected);
+  });
+});
+
+describe("get the min temperature of the day", () => {
+  it("returns 15 on the first day", () => {
+    const day = fakeForecastFetch.list.slice(8, 16);
+    const result = getMin(day);
+    const expected = 15
+    expect(result).toBe(expected);
+  });
+});
+
+describe("get the icon of the day", () => {
+  it("returns the 04n icon for the second day", () => {
+    const day2 = fakeForecastFetch.list.slice(8, 16);
+    const result = getIcon(day2);
+    const expected = '04n'
+    expect(result).toBe(expected);
+  });
+
+  it("returns the 01d icon for the third day", () => {
+    const day3 = fakeForecastFetch.list.slice(16, 24);
+    const result = getIcon(day3);
+    const expected = '01d'
+    expect(result).toBe(expected);
+  });
+
+  describe("format date", () => {
+    it("returns day and month", () => {
+      const date = fakeForecastFetch.list[0].dt_txt;
+      console.log(date)
+      const result = getDay(date);
+      const expected = '01-10';
+      expect(result).toBe(expected);
+    });
   });
 });
